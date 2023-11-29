fc1c100ff96a5edd3fafa0151df7150ab379e948
Brian Bud <hello@brianbud.com>
2023-11-28 20:56:04 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = `<h1>Hello World</h1>`;
+
+  return element;
+}
+
+document.body.appendChild(component());
